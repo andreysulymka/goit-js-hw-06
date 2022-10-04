06f499aae6dc3dbc0d14cad2d7ea4b1fb92402ae
andreysulymka <andreysulymka5@gmail.com>
2022-10-04 13:44:40 +0300

    add 5 tasks

--- js/task02.js
+++ js/task02.js
@@ -6,3 +6,26 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+
+const ingredientsContainerRef = document.querySelector('#ingredients');
+console.log(ingredientsContainerRef);
+const elements = [];
+
+for (let i = 0; i < ingredients.length; i += 1){
+  
+  const element = ingredients[i];
+  
+const itemRef = document.createElement('li');
+itemRef.textContent = ingredients[i];
+itemRef.classList.add('item');
+console.log(itemRef);
+  
+elements.push(itemRef);
+};
+console.log(elements);
+
+ingredientsContainerRef.append(...elements);
+
+
